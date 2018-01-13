13d5c5e1d587a75973a8f7b3800123ab2e8f430c
Sherif Ismail <sherifismail01@gmail.com>
2018-01-12 16:19:19 -0800

    styling

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -4,11 +4,18 @@ class PostController < ApplicationController
 		@cities = @user.cities
 	end
 
+	def show
+		post_id =params[:id]
+		@post = Post.find_by(id: post_id)
+		user_id = @post.user_id
+		@user = User.find_by(id: user_id)
+	end
+
 	def create 
 		if current_user.id === params[:post][:user_id]
 			Post.create(city_id: params[:post][:city_id], user_id: params[:post][:user_id])
 			redirect_to cities_path
-		
+		end
 	end
 
 end
